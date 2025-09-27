9eb34e70e91bfd80326c832ac542423766bdfaf5
infinity-blockchain-team <team@infinityblockchainsolutions.com>
2025-09-27 01:40:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -272,13 +272,14 @@ app.get('/api/init-admin', async (req, res) => {
 });
 
 
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   const PORT = process.env.PORT || 3000;
+//   app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+//   });
+// }
 
 
+export default app;
 
 
